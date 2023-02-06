4de359cd55a41c97f854ce07f9a1ec70ddc2a127
9ut4 <9ut4@fresnou.org>
2023-02-06 13:36:31 -0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,65 @@
+//Variable store data for later use
+
+//Create a variable with let
+let myVariable = "value";
+let myNewVariable = 7;
+let myBool = true;
+
+//Accessing variable values
+console.log(myVariable);
+console.log(myNewVariable);
+console.log(myBool);
+
+//Update value stored in variable
+myNewVariable=10;
+console.log(myNewVariable);
+
+//Increase by 1
+myNewVariable = myNewVariable+1;
+console.log(myNewVariable);
+
+//Decrease by 1
+myNewVariable = myNewVariable-1;
+console.log(myNewVariable);
+
+//Multiply by 3
+myNewVariable = myNewVariable*3;
+console.log(myNewVariable);
+
+//Divide by 3
+myNewVariable = myNewVariable/3;
+console.log(myNewVariable);
+
+//Divide and return the remainder
+myNewVariable = myNewVariable%2; //10/2=5 with 0 remainder
+console.log(myNewVariable);
+
+//Shortcuts!
+
+//add one
+let x=8;
+x++; //x=x+1;
+
+//subtract 1
+let y=3;
+y--; //y=y-1
+
+let z=24;
+z+=5 //z=z+5
+
+let a=10;
+a-=2 //a=a-2
+
+let b=10;
+b-=2 //a=a-2
+
+let c=10;
+c-=2 //a=a-2
+
+//Keywords
+const pi=3.14;
+const username="Mika";
+//constants can not be changed
+
+var old=123;
+//var is outdated and yucky. Don't use.
