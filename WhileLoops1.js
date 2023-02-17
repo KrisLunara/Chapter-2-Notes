9bf382dbc2223108d3be9d1be72d1be784312fb8
9ut4 <9ut4@fresnou.org>
2023-02-17 14:15:12 -0800

    commit 02-17-2023 - added While Loops

---
+++ WhileLoops1.js
@@ -0,0 +1,41 @@
+const readLine = require('readline-sync');
+
+// let x =0;
+// while (x<=20){
+//     console.log(x);
+//      x++;
+// }
+
+// let y =0;
+// while (y<=50){
+//     console.log(y);
+//     y++;
+// }
+
+// let z =-10;
+// while (z<=10){
+//     console.log(z);
+//     z++;
+// }
+
+// let a =0;
+// while (a>=-50){
+//     console.log(a);
+//     a--;
+// }
+
+// let b =0;
+// while (b<=100){
+//     if (b%2===0){
+//         console.log(b);
+//     }
+//     b++;
+// }
+
+let c =0;
+while (c<=100){
+    if (c%2!==0){
+        console.log(c);
+    }
+    c++;
+}
